7629d029d85d94a0f7edd9848ed18b501dd4fa82
Vladimir Dronnikov <dronnikov@gmail.com>
2012-01-17 12:54:35 +0000

    fixed test.lua, now it's failing...

--- test.lua
+++ test.lua
@@ -1,12 +1,14 @@
 #!/usr/bin/env luvit
 
+local String = require('string')
 local UTF8 = require('./')
 
 -- sanity --
-assert(UTF8)
 assert(not _G.unicode)
 assert(UTF8.sub)
 
-assert(UTF8.sub('Привет, Мир!', 2, 3), 'ри')
+--p(UTF8.sub('Привет, Мир!', 2, 3))
+assert(String.sub('Привет, Мир!', 2, 3) ~= 'ри')
+assert(UTF8.sub('Привет, Мир!', 2, 3) == 'ри')
 --p(UTF8, UTF8.len('Привет, Мир!'), #('Привет, Мир!'))
 assert(UTF8.len('Привет, Мир!') == 12)
